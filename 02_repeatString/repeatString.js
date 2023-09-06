11d614fb3f880a7c3a4b729ba1f1d578e66c5b45
Juneid Teklu <te.juneid@gmail.com>
2023-09-06 13:41:11 -0500

    Solve repeatString problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = '';
+    
+    if(num < 0){
+        repeatedString = 'ERROR';
+    }
+
+    for(let i = 0; i< num; i++){
+        repeatedString += string;
+    }
+    return repeatedString;
+
 
 };
 
