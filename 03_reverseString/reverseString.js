5d97e0ff73afd33115c5daf32cac7b478b72c9ba
Juneid Teklu <te.juneid@gmail.com>
2023-09-09 17:14:30 -0500

    Write function to reverse word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,27 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+let arrayWord = string.split('');
+let lengthOfWord = string.length;
+let reversedArray;
+let reversedWord;
+
+
+let i = 0;
+
+while(i < lengthOfWord - 1){
+reversedArray = arrayWord.splice(i,0, arrayWord[lengthOfWord-1]); 
+
+i++;
 };
+
+if (i == lengthOfWord-1){
+  reversedWord = reversedArray.toString();
+  return reversedWord;
+}
+
+}
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
