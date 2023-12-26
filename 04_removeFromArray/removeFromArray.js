cf869ae054fe4a2bffbf45006e1ff8317701fb62
Juneid Teklu <te.juneid@gmail.com>
2023-12-26 17:56:50 -0600

    Write a note to myself

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
 const removeFromArray = function(array, removedElement) {
 
-    let newArray = array.filter((element) => element != removedElement);
+    let newArray = array.filter((element) => element != removedElement); /*maybe instead of element != removedELement, it needs to check each array element of removedELement. Because right now, I think its saying that removedELement is all one unit when it needs to see each number of that array as its own unit. */
     return newArray;
 
 };
