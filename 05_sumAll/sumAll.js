460d7f075a6cb05d40e7c25cab17032b7116e943
Juneid Teklu <te.juneid@gmail.com>
2024-02-07 17:11:03 -0600

    Write if else statement and loop to icrememnt the smalles number and fail

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
 
-};
+sumAll(1,4);
+
+const sumAll = function(num1, num2) {
+
+    let sumOfNumbers = 0;
+    if (num1 > num2){
+        for (let i = num2; i <= num1; i++){
+            sumOfNumbers += ++i;
+        }
+    }
+    else {
+        for (let i = num1; i <= num2; i++){
+            sumOfNumbers += ++i;
+
+    }
+
+}
+return sumOfNumbers;
+}
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
+
