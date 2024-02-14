c7a2118b1f7083ad863a67457f2b5e1bf61f0853
Juneid Teklu <te.juneid@gmail.com>
2024-02-13 18:42:49 -0600

    Add if statement to check if arguments are a number and non-negative

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,23 +1,30 @@
 
-sumAll(1,4);
-
 const sumAll = function(num1, num2) {
 
     let sumOfNumbers = 0;
-    if (num1 > num2){
-        for (let i = num2; i <= num1; i++){
-            sumOfNumbers += ++i;
+    if(typeof num1 == "number" && typeof num2 == "number" && num1 >= 0 && num2 >= 0){
+            if (num1 > num2){
+                for (let i = num2; i <= num1; i){
+                    sumOfNumbers += i++;
+                    
+                    
+                }
+            }
+            else {
+                for (let i = num1; i <= num2; i){
+                    sumOfNumbers += i++;
+
+            }
+
         }
     }
-    else {
-        for (let i = num1; i <= num2; i++){
-            sumOfNumbers += ++i;
+    else {sumOfNumbers = "ERROR"}
 
-    }
-
-}
 return sumOfNumbers;
 }
+
+
+sumAll(1,4);
 
 
 
