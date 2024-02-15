9853f744bf06c1e46a081efcc896779a1d697e49
Juneid Teklu <te.juneid@gmail.com>
2024-02-14 18:21:03 -0600

    Solve problem

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,25 @@
-const leapYears = function() {
+const leapYears = function(givenYear) {
+    let firstCheck = givenYear/4;
+    if (Number.isInteger(givenYear/100)){
+        switch (Number.isInteger(givenYear/400)){
+            case Boolean(1):
+                return Boolean(1);
+                break;
+            case Boolean(0):
+                return Boolean(0);
+                break;
+        }
+    }
+    else {
+        switch (Number.isInteger(firstCheck)){
+            case Boolean(1):
+                return Boolean(1);
+                break;
+            case Boolean(0):
+                return Boolean(0);
+                break;
+        }
+    }
 
 };
 
